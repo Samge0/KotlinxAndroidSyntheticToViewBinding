c488ee42a0ac4b0a447805b1da872e5727aa9b6d
shaochengbao <samge720@163.com>
2024-03-08 16:46:48 +0800

    update replace rule

--- src/main/kotlin/com/github/samge0/KotlinxAndroidSyntheticToViewBinding/ext/String.kt
+++ src/main/kotlin/com/github/samge0/KotlinxAndroidSyntheticToViewBinding/ext/String.kt
@@ -6,23 +6,23 @@ fun String.toCamelCase():  String {
 
     // 对包含下划线的字符串进行处理
     return split("_").mapIndexed { index, s ->
-        if (index > 0) s.replaceFirstChar { it.uppercase() } else s.lowercase()
+        if (index > 0) s.replaceFirstChar { it.uppercase() } else s.replaceFirstChar{ it.lowercase() }
     }.joinToString("")
 }
 
-fun String.toCapitalCase(): String = split("_").joinToString(" ") { it.lowercase().replaceFirstChar { char -> char.uppercase() } }
+fun String.toCapitalCase(): String = split("_").joinToString(" ") { it.replaceFirstChar { char -> char.uppercase() } }
 
 fun String.toConstantCase(): String = uppercase().replace(" ", "_")
 
 fun String.toDotCase(): String = lowercase().replace(" ", ".")
 
-fun String.toHeaderCase(): String = split(" ").joinToString("-") { it.lowercase().replaceFirstChar { char -> char.uppercase() } }
+fun String.toHeaderCase(): String = split(" ").joinToString("-") { it.replaceFirstChar { char -> char.uppercase() } }
 
 fun String.toNoCase(): String = lowercase().replace("_", " ")
 
 fun String.toParamCase(): String = lowercase().replace(" ", "-")
 
-fun String.toPascalCase(): String = split("_").joinToString("") { it.lowercase().replaceFirstChar { char -> char.uppercase() } }
+fun String.toPascalCase(): String = split("_").joinToString("") { it.replaceFirstChar { char -> char.uppercase() } }
 
 fun String.toPathCase(): String = lowercase().replace(" ", "/")
 
